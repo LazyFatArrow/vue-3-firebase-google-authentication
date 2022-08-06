710504a83a837d1084dec22b3e32bedd46416c20
amenallah.hsoumi <amenallah.hsoumi@check24.de>
2022-08-06 15:47:41 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  azeaze
+</template>
+
+<style>
+</style>
---
+++ src/services/firebase.service.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID
+};
+
+let app
+
+export const setup = () => { app = initializeApp(firebaseConfig) }
+
+export {
+  app
+}
